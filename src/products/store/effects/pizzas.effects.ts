877181f036e894cbdddc549eef72afc31a6695f4
Alexis <alexisvt@gmail.com>
2018-02-12 22:11:15 -0600

    adding create effect and reducer

--- src/products/store/effects/pizzas.effects.ts
+++ src/products/store/effects/pizzas.effects.ts
@@ -18,6 +18,19 @@ export class PizzasEffects {
         );
     })
   );
+
+  @Effect()
+  createPizza$ = this.actions$.ofType(pizzaActions.CREATE_PIZZA).pipe(
+    map((action: pizzaActions.CreatePizza) => action.payload),
+    switchMap(pizza => {
+      return this.pizzasService
+        .createPizza(pizza)
+        .pipe(
+          map(pizza => new pizzaActions.CreatePizzaSuccess(pizza)),
+          catchError(error => of(new pizzaActions.CreatePizzaFail(error)))
+        );
+    })
+  );
   constructor(
     private actions$: Actions,
     private pizzasService: fromServices.PizzasService
